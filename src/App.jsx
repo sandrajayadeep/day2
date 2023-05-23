4d5f6b503d8dd93b3d2c8a8ff0097c48acfbf488
sandrajayadeep <sandrajayadeep01@gmail.com>
2023-05-23 17:01:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      {count > 0 && <button onClick={decrement}>Decrement</button>}
+    </div>
+  );
+};
+
+export default Counter;
